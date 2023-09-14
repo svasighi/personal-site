d80db63cde9b0e005f2ce3cdd292a82df5a94e92
shadmehr vasighi <shadmehr.vasighi99@gmail.com>
2023-09-14 11:13:13 +0330

    chore(codebase): installing prettier

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,20 +5,20 @@ import { Inter } from 'next/font/google';
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-  title:
-    'Shadmehr Vasighi - Front-End Developer specializing in React and Next.js',
-  description:
-    'Shadmehr Vasighi, a Front-End Developer with expertise in building websites using React and Next.js. With a focus on TypeScript and visual interface design, I am ready to collaborate on high-performance website development projects with excellent user experience.',
+	title:
+		'Shadmehr Vasighi - Front-End Developer specializing in React and Next.js',
+	description:
+		'Shadmehr Vasighi, a Front-End Developer with expertise in building websites using React and Next.js. With a focus on TypeScript and visual interface design, I am ready to collaborate on high-performance website development projects with excellent user experience.',
 };
 
 export default function RootLayout({
-  children,
+	children,
 }: {
-  children: React.ReactNode;
+	children: React.ReactNode;
 }) {
-  return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
-  );
+	return (
+		<html lang='en'>
+			<body className={inter.className}>{children}</body>
+		</html>
+	);
 }
