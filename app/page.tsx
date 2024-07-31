aa3ce43f1326c7e5147c7a4493729e32c483e880
shadmehr vsighi <shadmehr.vasighi99@gmail.com>
2024-07-31 20:14:08 +0330

    feat: contact button

--- app/page.tsx
+++ app/page.tsx
@@ -8,6 +8,8 @@ import {
 	Testimonials,
 } from '@sections/index';
 import SoftScroll from '@/src/components/SoftScroll';
+import { HeartIcon } from '@heroicons/react/24/outline';
+import { PhoneIcon } from '@heroicons/react/24/solid';
 
 //Todo move stylesheet to it's module
 export default function Home() {
@@ -34,7 +36,15 @@ export default function Home() {
 						</div>
 					</div>
 				</SoftScroll>
-				<div className='relative top-12 hidden h-80 w-14 flex-col items-start justify-start gap-8 rounded-3xl border border-gray-400 bg-gray-50 bg-opacity-10 px-4 py-8 backdrop-blur-lg md:top-16 md:hidden lg:top-20 lg:inline-flex'></div>
+				<a
+					href=' tel:+989196850370'
+					className='lg:flex-0 group fixed bottom-10 mt-20 h-fit w-fit cursor-pointer gap-2 rounded-2xl bg-slate-800 p-4 text-slate-200 shadow-2xl shadow-slate-700 transition-all duration-500 ease-in-out hover:flex hover:shadow-xl hover:shadow-slate-600 lg:static lg:flex'
+				>
+					<PhoneIcon width={20} />
+					<p className='hidden hover:block group-hover:block lg:block'>
+						+989196850370
+					</p>
+				</a>
 			</div>
 			<div className='absolute  right-8 top-20 -z-10 h-16 w-16 rounded-full bg-pink-500 blur-3xl'></div>
 			<div className='absolute -top-12 right-32 -z-10  h-32 w-32 rounded-full bg-yellow-500 blur-3xl'></div>
